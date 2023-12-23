b552626fc754a3010d40f0e3b4ac8168c017216c
BenAichaMed <benaichamed66@gmail.com>
2023-12-23 16:43:44 +0100

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
